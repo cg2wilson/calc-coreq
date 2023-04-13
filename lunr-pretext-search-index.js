9684ea44e625ce21dcc4a21a75c032687929723e
Cory Wilson <cory.g.wilson@gmail.com>
2023-04-13 18:46:14 -0500

    Latest build of target web.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1",
   "title": "A. Function Notation and Evaluation",
-  "body": " A. Function Notation and Evaluation   Function Notation   Supplements XYZ Sections: All    Function Notation Intro    A function is a relation that assigns exactly one output to every input. The set of inputs is the domain of the function. The set of outputs is the range of the function.  For a function :    is the function name     is the input     is the output or value for the corresponding      An important thing to notet is that does not indicate multiplication between and . Rather, represents the output or value obtained by plugging the corresponding value into the function . A function can be represented algebraically (with a formula), graphically (with a graph), numerically (with a table), or verbally (with a description.)   A diagram describing the \"function machine\"- is input into the machine called , and produced the output     Another important observation is that we can use variables other than and !  For example, let's identify the function name, input variable, and output variable in the following expressions: Let's try one more!   Some different types of functions with function notation:    Linear Function:  Is this different than ?    Quadratic Function:  Is the same as ?            "
+  "body": " A. Function Notation and Evaluation   Function Notation   Supplements XYZ Sections: All    Function Notation Intro     Video   A function is a relation that assigns exactly one output to every input. The set of inputs is the domain of the function. The set of outputs is the range of the function.  For a function :    is the function name     is the input     is the output or value for the corresponding      An important thing to notet is that does not indicate multiplication between and . Rather, represents the output or value obtained by plugging the corresponding value into the function . A function can be represented algebraically (with a formula), graphically (with a graph), numerically (with a table), or verbally (with a description.)   A diagram describing the \"function machine\"- is input into the machine called , and produced the output     Another important observation is that we can use variables other than and !  For example, let's identify the function name, input variable, and output variable in the following expressions: Let's try one more!   Some different types of functions with function notation:    Linear Function:  Is this different than ?    Quadratic Function:  Is the same as ?        Identify the function name, input variable, and output variable:       Function name:     Input variable:     Output variable:               True or False: We can divide both sides of the above equation by to get     What is the input variable?    What is the output variable?          False: is not times . Rather, is function notation for the output or value. Another notation for the given relation would be . is equivalent to the value, just in a different notation. Dividing both sides of the equation by is akin to dividing the function by itself.                    Function Evaluation   Supplements XYZ Sections: All    Function Evaluation, Algebraic     Video      Find given that     Find given that     Find given that                                  Find given that     Find given that     Find given that                               Function Evaluation, Algebraic Piecewise     Video    Find , and      , , and        Find , and      , , and       Function Evaluation, Graphical     Video   The graph of is given.   Find , , , , and      , , , , DNE      The graph of is given. Find , , , , and .      , , , , and .      Function Evaluation, Numerical     Video      1  2  3  4  5      3.7  4.2  3.9  7.4     0  0  5  0     Find , , , and      , , , and        Video       1  3  5  7     7  6  5  4  3      15.9   5.3     Find , , , and .     , , , and .      Function Evaluation, Symbolic     Video    . Find , , , and .     , , , and .       . Find the following:                                                        "
 },
 {
   "id": "example-1",
@@ -16,7 +16,250 @@ var ptx_lunr_docs = [
   "type": "Example",
   "number": "1.1.1",
   "title": "",
-  "body": "  A function is a relation that assigns exactly one output to every input. The set of inputs is the domain of the function. The set of outputs is the range of the function.  For a function :    is the function name     is the input     is the output or value for the corresponding      An important thing to notet is that does not indicate multiplication between and . Rather, represents the output or value obtained by plugging the corresponding value into the function . A function can be represented algebraically (with a formula), graphically (with a graph), numerically (with a table), or verbally (with a description.)   A diagram describing the \"function machine\"- is input into the machine called , and produced the output     Another important observation is that we can use variables other than and !  For example, let's identify the function name, input variable, and output variable in the following expressions: Let's try one more!   Some different types of functions with function notation:    Linear Function:  Is this different than ?    Quadratic Function:  Is the same as ?         "
+  "body": "   Video   A function is a relation that assigns exactly one output to every input. The set of inputs is the domain of the function. The set of outputs is the range of the function.  For a function :    is the function name     is the input     is the output or value for the corresponding      An important thing to notet is that does not indicate multiplication between and . Rather, represents the output or value obtained by plugging the corresponding value into the function . A function can be represented algebraically (with a formula), graphically (with a graph), numerically (with a table), or verbally (with a description.)   A diagram describing the \"function machine\"- is input into the machine called , and produced the output     Another important observation is that we can use variables other than and !  For example, let's identify the function name, input variable, and output variable in the following expressions: Let's try one more!   Some different types of functions with function notation:    Linear Function:  Is this different than ?    Quadratic Function:  Is the same as ?     "
+},
+{
+  "id": "example-2",
+  "level": "2",
+  "url": "a.html#example-2",
+  "type": "Example",
+  "number": "1.1.2",
+  "title": "",
+  "body": "  Identify the function name, input variable, and output variable:       Function name:     Input variable:     Output variable:      "
+},
+{
+  "id": "example-3",
+  "level": "2",
+  "url": "a.html#example-3",
+  "type": "Example",
+  "number": "1.1.3",
+  "title": "",
+  "body": "        True or False: We can divide both sides of the above equation by to get     What is the input variable?    What is the output variable?          False: is not times . Rather, is function notation for the output or value. Another notation for the given relation would be . is equivalent to the value, just in a different notation. Dividing both sides of the equation by is akin to dividing the function by itself.                "
+},
+{
+  "id": "example-4",
+  "level": "2",
+  "url": "a.html#example-4",
+  "type": "Example",
+  "number": "1.2.1",
+  "title": "",
+  "body": "   Video      Find given that     Find given that     Find given that                            "
+},
+{
+  "id": "example-5",
+  "level": "2",
+  "url": "a.html#example-5",
+  "type": "Example",
+  "number": "1.2.2",
+  "title": "",
+  "body": "     Find given that     Find given that     Find given that                            "
+},
+{
+  "id": "example-6",
+  "level": "2",
+  "url": "a.html#example-6",
+  "type": "Example",
+  "number": "1.2.3",
+  "title": "",
+  "body": "   Video    Find , and      , , and    "
+},
+{
+  "id": "example-7",
+  "level": "2",
+  "url": "a.html#example-7",
+  "type": "Example",
+  "number": "1.2.4",
+  "title": "",
+  "body": "   Find , and      , , and    "
+},
+{
+  "id": "example-8",
+  "level": "2",
+  "url": "a.html#example-8",
+  "type": "Example",
+  "number": "1.2.5",
+  "title": "",
+  "body": "   Video   The graph of is given.   Find , , , , and      , , , , DNE   "
+},
+{
+  "id": "example-9",
+  "level": "2",
+  "url": "a.html#example-9",
+  "type": "Example",
+  "number": "1.2.6",
+  "title": "",
+  "body": "  The graph of is given. Find , , , , and .      , , , , and .   "
+},
+{
+  "id": "example-10",
+  "level": "2",
+  "url": "a.html#example-10",
+  "type": "Example",
+  "number": "1.2.7",
+  "title": "",
+  "body": "   Video      1  2  3  4  5      3.7  4.2  3.9  7.4     0  0  5  0     Find , , , and      , , , and    "
+},
+{
+  "id": "example-11",
+  "level": "2",
+  "url": "a.html#example-11",
+  "type": "Example",
+  "number": "1.2.8",
+  "title": "",
+  "body": "   Video       1  3  5  7     7  6  5  4  3      15.9   5.3     Find , , , and .     , , , and .   "
+},
+{
+  "id": "example-12",
+  "level": "2",
+  "url": "a.html#example-12",
+  "type": "Example",
+  "number": "1.2.9",
+  "title": "",
+  "body": "   Video    . Find , , , and .     , , , and .   "
+},
+{
+  "id": "example-13",
+  "level": "2",
+  "url": "a.html#example-13",
+  "type": "Example",
+  "number": "1.2.10",
+  "title": "",
+  "body": "   . Find the following:                                                     "
+},
+{
+  "id": "b",
+  "level": "1",
+  "url": "b.html",
+  "type": "Section",
+  "number": "2",
+  "title": "B. Lines and Solving Linear Equations",
+  "body": " B. Lines and Solving Linear Equations   Lines   Supplements XYZ Sections: All    Slope     Video   The slope of a line measures the rate of change of the line (rise over run). Given two points on a line and , we can calculate the slope using the following:   Find the slope of the line that goes through the points and .  We can also find the slope of a line from its graph.       Find the slope of the line that goes through the points and .           What is the slope of the line graphed below?            Slope-Intercept Form     Video   The equation of a line in slope-intercept form is: where is the slope and is intercept  Identify the slope and intercept for . Sketch the graph of . Is the graph of an increasing, decreasing, or horizontal line?     The slope of an increasing line is positive     The slope of a decreasing line is negatitve     The slope of a horizontal line is zero     The slope of a vertical line is undefined        The slope is 2 and the intercept is . The graph is an increasing line:   The graph of on         Identify the slope and intercept of . Sketch the graph of . Is the graph of an increasing, decreasing, or horizontal line?    The slope is and the intercept is . The graph is a decreasing line:       Identify the slope and intercept of . Sketch the graph of . Is an increasing, decreasing, or horizontal line?    The slope is 0 and the intercept is . The graph is a horizontal line:       Finding an Equation of a Line in Slope-Intercept Form     Video   Find the equation of the line that passes through the points and in slope-intercept form.           Find the equation of the line that passes through the points and in slope-intercept form.           Find the equation of the line that passes through the points and in slope-intercept form.           Point-Slope Form     Video   The equation of a line in point-slope form is: where is the slope and is any point on the line.  Identify the slope of . What point on can we identify without any calculation? Rewrite in slope-intercept form and identify the intercept    The slope is 3, and the point is . In slope intercept form, the line is and the intercept is       For , (a) identify the slope, (b) identify a point on without doing any calculations, and (c) rewrite in slope-intercept form       2                   Find the equation of the line with slope that passes through the point in point-slope form.            Solving Linear Equations     Video   Solve the following linear equations for                                                         Solve the following linear equations for                                              "
+},
+{
+  "id": "example-14",
+  "level": "2",
+  "url": "b.html#example-14",
+  "type": "Example",
+  "number": "2.1.1",
+  "title": "",
+  "body": "   Video   The slope of a line measures the rate of change of the line (rise over run). Given two points on a line and , we can calculate the slope using the following:   Find the slope of the line that goes through the points and .  We can also find the slope of a line from its graph.    "
+},
+{
+  "id": "example-15",
+  "level": "2",
+  "url": "b.html#example-15",
+  "type": "Example",
+  "number": "2.1.2",
+  "title": "",
+  "body": "  Find the slope of the line that goes through the points and .        "
+},
+{
+  "id": "example-16",
+  "level": "2",
+  "url": "b.html#example-16",
+  "type": "Example",
+  "number": "2.1.3",
+  "title": "",
+  "body": "  What is the slope of the line graphed below?         "
+},
+{
+  "id": "example-17",
+  "level": "2",
+  "url": "b.html#example-17",
+  "type": "Example",
+  "number": "2.1.4",
+  "title": "",
+  "body": "   Video   The equation of a line in slope-intercept form is: where is the slope and is intercept  Identify the slope and intercept for . Sketch the graph of . Is the graph of an increasing, decreasing, or horizontal line?     The slope of an increasing line is positive     The slope of a decreasing line is negatitve     The slope of a horizontal line is zero     The slope of a vertical line is undefined        The slope is 2 and the intercept is . The graph is an increasing line:   The graph of on      "
+},
+{
+  "id": "example-18",
+  "level": "2",
+  "url": "b.html#example-18",
+  "type": "Example",
+  "number": "2.1.5",
+  "title": "",
+  "body": "  Identify the slope and intercept of . Sketch the graph of . Is the graph of an increasing, decreasing, or horizontal line?    The slope is and the intercept is . The graph is a decreasing line:    "
+},
+{
+  "id": "example-19",
+  "level": "2",
+  "url": "b.html#example-19",
+  "type": "Example",
+  "number": "2.1.6",
+  "title": "",
+  "body": "  Identify the slope and intercept of . Sketch the graph of . Is an increasing, decreasing, or horizontal line?    The slope is 0 and the intercept is . The graph is a horizontal line:    "
+},
+{
+  "id": "example-20",
+  "level": "2",
+  "url": "b.html#example-20",
+  "type": "Example",
+  "number": "2.1.7",
+  "title": "",
+  "body": "   Video   Find the equation of the line that passes through the points and in slope-intercept form.        "
+},
+{
+  "id": "example-21",
+  "level": "2",
+  "url": "b.html#example-21",
+  "type": "Example",
+  "number": "2.1.8",
+  "title": "",
+  "body": "  Find the equation of the line that passes through the points and in slope-intercept form.        "
+},
+{
+  "id": "example-22",
+  "level": "2",
+  "url": "b.html#example-22",
+  "type": "Example",
+  "number": "2.1.9",
+  "title": "",
+  "body": "  Find the equation of the line that passes through the points and in slope-intercept form.        "
+},
+{
+  "id": "example-23",
+  "level": "2",
+  "url": "b.html#example-23",
+  "type": "Example",
+  "number": "2.1.10",
+  "title": "",
+  "body": "   Video   The equation of a line in point-slope form is: where is the slope and is any point on the line.  Identify the slope of . What point on can we identify without any calculation? Rewrite in slope-intercept form and identify the intercept    The slope is 3, and the point is . In slope intercept form, the line is and the intercept is    "
+},
+{
+  "id": "example-24",
+  "level": "2",
+  "url": "b.html#example-24",
+  "type": "Example",
+  "number": "2.1.11",
+  "title": "",
+  "body": "  For , (a) identify the slope, (b) identify a point on without doing any calculations, and (c) rewrite in slope-intercept form       2                "
+},
+{
+  "id": "example-25",
+  "level": "2",
+  "url": "b.html#example-25",
+  "type": "Example",
+  "number": "2.1.12",
+  "title": "",
+  "body": "  Find the equation of the line with slope that passes through the point in point-slope form.        "
+},
+{
+  "id": "example-26",
+  "level": "2",
+  "url": "b.html#example-26",
+  "type": "Example",
+  "number": "2.2.1",
+  "title": "",
+  "body": "   Video   Solve the following linear equations for                                                      "
+},
+{
+  "id": "example-27",
+  "level": "2",
+  "url": "b.html#example-27",
+  "type": "Example",
+  "number": "2.2.2",
+  "title": "",
+  "body": "  Solve the following linear equations for                                            "
 }
 ]
 
